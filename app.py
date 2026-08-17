8a0639248d288ab3739545076ec1f9fbab4973e0
Leka <leka@zymber.tech>
2026-08-17 18:00:53 +0800

    ADD initial Fate Ball wed page

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask import request
 from ball_body import fate
+from flask import render_template
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route("/api/health")
 def health_check():
